18a5a4a7bd086ff382d3de041b8a9e133af5b738
deathboydmi <smertindmi@mail.ru>
2018-11-27 16:08:29 +0300

    init2scripts

---
+++ scripts/nodules_gen.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import cv2 as cv
+import SimpleITK as itk
+
+import os
+import zipfile as zip
+
+
+def load_candidates(path):
+    labels = pd.read_csv(path)
+    labels = pd.read_csv(path)
+    labels['seriesuid'] = labels['seriesuid'].astype('str')
+    labels['label'] = labels['label'].astype('bool')
+    labels.head()
+    return labels
+
+def get_info(row):
+    path = "./data/" + row['seriesuid'] + ".mhd"
+    x, y, z = row['coordX'], row['coordY'], row['coordZ']
+    return path, x, y, z
+
+def get_contain(path):
+    listdir = os.listdir(path)
+    data_field = []
+    for zip_path in listdir:
+        z = zip.ZipFile(path+zip_path)
+        data_field.append(z.namelist())
+        z.close()
+    print(data_field)
+    data_field = (np.ndarray(data_field, dtype='str')).transpose()
+    data_frame = pd.DataFrame(data_field[1:,:], columns=data_field[:1,:])
+    data_frame.to_csv("./data_frame.csv")
+    return data_frame
+
+
+#def load_model(name, path):
+
+print(get_contain("../data/"))
